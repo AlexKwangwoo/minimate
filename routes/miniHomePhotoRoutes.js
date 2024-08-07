065c0345b9d4fb9b3141f3d398f29debf2e5151d
Kwangwoo Alex <bnc3049@gmail.com>
2024-08-07 17:40:23 -0600

    Pushed photo comment

--- routes/miniHomePhotoRoutes.js
+++ routes/miniHomePhotoRoutes.js
@@ -12,6 +12,16 @@ router
     miniHomePhotoController.updatePictureToPhotoFolder
   );
 
-router.route('/:id').delete(miniHomePhotoController.deleteMiniHomePhoto);
+router
+  .route('/:id')
+  .patch(miniHomePhotoController.updatePhotoInside)
+  .delete(miniHomePhotoController.deleteMiniHomePhoto);
+
+router.route('/:id/comment').post(miniHomePhotoController.addComment);
+
+router
+  .route('/:id/comment/:commentId')
+  .patch(miniHomePhotoController.updatePhotoComment)
+  .delete(miniHomePhotoController.deletePhotoComment);
 
 module.exports = router;
